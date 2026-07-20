34bf8daf3879f20f4b5079a5fc01e1595ea51550
DHJComical <dhjcomical@163.com>
2026-07-20 17:44:36 +0800

    chore: switch to GTNH 1.7.10 template baseline

--- src/main/java/neofontrender/Tags.java
+++
@@ -1,7 +0,0 @@
-package neofontrender;
-
-public class Tags {
-    public static final String VERSION = "0.3.1";
-    public static final String MOD_ID = "neofontrender";
-    public static final String MOD_NAME = "Neo Font Render";
-}
